cd2438b30d374eb1253b8a7665bd94f315ae2c7c
Jon Thompson <sirjdt@gmail.com>
2018-04-23 19:06:49 -0500

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = '|z\xa0!\xc0n\xb6\x86J\xc9G\xaa\r\x06R5 Y\xdf\x19\xba*\xde\xcd'
@@ -14,10 +14,12 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
     def validate(self):
         if self.title and self.body:
@@ -25,14 +27,27 @@ class Blog(db.Model):
         else:
             return False
 
+class User(db.Model):
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
 
-@app.route('/', methods=['POST', 'GET'])
-def blog():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-    entries = Blog.query.all()
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register', 'blogs' , 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
 
-    return render_template('blog.html', title=blog, entries=entries)
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', title=blog, users=users)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
@@ -40,7 +55,8 @@ def newpost():
     if request.method == 'POST':
         blog_title = request.form['blog']
         blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
+        owner = User.query.filter_by(username=session['username']).first()
+        new_blog = Blog(blog_title, blog_body, owner)
         title_error = ''
         blog_error = ''
 
@@ -58,7 +74,8 @@ def newpost():
             flash("Please enter a blog title", 'error')
             return render_template('newpost.html', blog_title=blog_title)
         else:
-            return redirect("/blog?id={}".format(blog_id))
+            return redirect('/blogpost?id={0}&user={1}'.format(blog_id, owner))
+
         
     else:
         return render_template('newpost.html')
